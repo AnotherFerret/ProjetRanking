5a0863baf9f9bc02d9b2f7d85bb895113c65b7c2
Raphael_home <raphael.duprat@hotmail.com>
2020-04-11 17:48:06 +0200

    nettoyage du code

--- main.cpp
+++ main.cpp
@@ -261,14 +261,16 @@ public:
 		}
 	}
 
-	void read(Matrice *m) {
+	void read(Matrice *m) 
+	{
 		readHeader();
 		int off, n;
 		int c;
 		int x, y;
 		double valeur;
 		std::string buffer;
-		if(order == 0){
+		if(order == 0)
+		{
 			while (!file.eof())
 			{
 				getline(file, buffer);
@@ -284,8 +286,10 @@ public:
 					m->inserer(x - offset, y - offset, valeur);
 				}
 			}
-		}else{
-			while (!file.eof())
+		}
+		else
+		{
+		while (!file.eof())
 			{
 				getline(file, buffer);
 				off = 0;
@@ -295,7 +299,8 @@ public:
 				std::sscanf(buffer.c_str() + off, "%d%n", &c, &n);
 				off += n;
 				//std::cout << x << std::endl;
-				while (2 == std::sscanf(buffer.c_str() + off, "%lf%d%n", &valeur, &y, &n)) {
+				while (2 == std::sscanf(buffer.c_str() + off, "%lf%d%n", &valeur, &y, &n)) 
+				{
 					off += n;
 					m->inserer(x - offset, y - offset, valeur);
 				}
@@ -323,29 +328,6 @@ double* diff_vect(double * vect1, double * vect2, int size){
 }
 
 //
-void show_vecteur(double* vecteur, int size)
-{
-	int i = 0;
-	
-	for(i = 0; i < size; i++)
-	{
-		cout << vecteur[i] << " ";
-	}
-	cout << endl;
-}
-
-
-double* init_vecteur(int size, double value)
-{
-	double* vecteur = new double[size];
-	for(int j = 0; j < size; j++)
-		{
-			vecteur[j] = value;
-		}
-	return vecteur;
-}
-
-
 //
 
 double calcul_norme(double* vecteur, int size)
@@ -374,128 +356,6 @@ double calcul_norme(double* vecteur1, double* vecteur2, int size)
 	return result;
 }
 
-//fonction à modifier pour prendre en compte le surfer ( et initialiser G en même temps ? ) 
-void fill_vecteur_min(double* vecteur, string line, int current_line)
-{
-	int i = 0;
-	int reading = 0;
-	
-	//int line_size = 0;
-	double tmp_double = 0.0;
-	
-	string current = "";
-	
-	while(line[i] != '\n' && line[i] != '\0')
-	{
-		while(line[i] == ' ' && line[i] != '\0')
-		{
-			i++;
-		}
-		while((line[i] >= '0' && line[i] <= '9') || line[i] == 'E' || line[i] == '-' || line[i] == '.')
-		{
-			current += line[i];
-			i++;
-		}
-		
-		//cout << current << "\n";
-		if(reading == 0)
-		{
-			//ignore le premier chiffre
-			reading = 1;
-		}
-		else if(reading == 1)
-		{
-			//stocke la taille de la ligne
-			reading = 2;
-			//line_size = stoi(current);
-		}
-		else if(reading == 2)
-		{
-			//recupere la valeur double
-			reading = 3;
-			if( !current.empty() ){tmp_double = stod(current);}
-							
-		}
-		else if(reading == 3)
-		{
-			//ecrit la valeur double dans la case suivante
-			if(tmp_double < vecteur[current_line] || vecteur[current_line] == 0.0)
-			{
-				vecteur[current_line] = tmp_double;
-			}
-			reading = 2;
-		}
-		i++;
-		current = "";
-	}
-}
-
-//fonction à modifier pour prendre en compte le surfer
-void fill_vecteur_max(double* vecteur, string line, int current_line)
-{
-	int i = 0;
-	int reading = 0;
-	
-	//int line_size = 0;
-	double tmp_double = 0.0;
-	
-	string current = "";
-	
-	while(line[i] != '\n' && line[i] != '\0')
-	{
-		while(line[i] == ' ' && line[i] != '\0')
-		{
-			i++;
-		}
-		while((line[i] >= '0' && line[i] <= '9') || line[i] == 'E' || line[i] == '-' || line[i] == '.')
-		{
-			current += line[i];
-			i++;
-		}
-		
-		//cout << current << "\n";
-		if(reading == 0)
-		{
-			//ignore le premier chiffre
-			reading = 1;
-		}
-		else if(reading == 1)
-		{
-			//stocke la taille de la ligne
-			reading = 2;
-			//line_size = stoi(current);
-		}
-		else if(reading == 2)
-		{
-			//recupere la valeur double
-			reading = 3;
-			if( !current.empty() ){tmp_double = stod(current);}
-							
-		}
-		else if(reading == 3)
-		{
-			//ecrit la valeur double dans la case suivante
-			if(tmp_double > vecteur[current_line] || vecteur[current_line] == 0.0)
-			{
-				vecteur[current_line] = tmp_double;
-			}
-			reading = 2;
-		}
-		i++;
-		current = "";
-	}
-}
-
-double* produit_nabla_norme(double* nabla, double Xk, int size)
-{
-	double* result = init_vecteur(size, 0.0);
-	for(int i = 0; i<size; i++)
-	{
-		result[i] = nabla[i] * Xk;
-	}
-	return result;
-}
-
 void produit_nabla_norme(double* nabla, double Xk, int size, double * nablap)
 {
 	for(int i = 0; i<size; i++)
@@ -506,63 +366,23 @@ void produit_nabla_norme(double* nabla, double Xk, int size, double * nablap)
 
 int main(int argc, char **argv)
 {
-	string line;
-	ifstream file(argv[1]);
-	
-	int line_number = 0;
-	int current_line = 0;
-	double norme_x = 0;
-	double norme_y = 0;
+	double epsilon = 1e-6;
 
-	
 	//init matrice web
 	Reader r(argv[1],atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
 	r.readHeader();
 	MatriceG matrice(r.getNbSommets(), EPS);
 	r.read(&matrice);
 	
-	//variables tests à supprimer au final
+	//init vecteurs utilisés
 	double* vecteur_max_delta = matrice.getVecteurMax();
 	double* vecteur_min_nabla = matrice.getVecteurMin();
-
 	double* vecteur_xkg = new double[matrice.size()];
 	double* vecteur_ykg = new double[matrice.size()];
-
 	double* vecteur_x = NULL;
 	double* vecteur_y = NULL;
-	
-	/*if(file)
-	{
-		while(getline(file, line))
-		{
-			if(current_line == 0)
-			{
-				
-			}
-			else if(current_line == 1)
-			{
-				line_number = stoi(line);
-				vecteur_max_delta = init_vecteur(line_number, 0.0);
-				vecteur_min_nabla = init_vecteur(line_number, 0.0);
-				vecteur_x = init_vecteur(line_number, 0.0);
-				vecteur_y = init_vecteur(line_number, 0.0);
-			}
-			else if(current_line >= 2)
-			{
-				fill_vecteur_min(vecteur_min_nabla, line, current_line-2);
-				fill_vecteur_max(vecteur_max_delta, line, current_line-2);
-			}
-			current_line++;
-		}	
-	}
-	
-	//init X^0 Y^0
-	
-	show_vecteur(vecteur_x, line_number);
-	cout << endl;
-	show_vecteur(vecteur_y, line_number);
-	cout << endl;*/
 
+	//affectation X^0 Y^0
 	vecteur_x = matrice.getVecteurMin();
 	vecteur_y = matrice.getVecteurMax();
 	
@@ -571,16 +391,19 @@ int main(int argc, char **argv)
 	double * nablax = new double[matrice.size()];
 	double * nablay = new double[matrice.size()];
 	int n = matrice.size();
-	//future boucle tant que calcul_norme(difference(vecteur_x, vecteur_y)) > epsilon
+
 	cout << "taille : "<< n << ":: résidus : " << reste << endl;
-	while((reste = calcul_norme(vecteur_x, vecteur_y, n)) > 1e-6){
+	while((reste = calcul_norme(vecteur_x, vecteur_y, n)) > epsilon)
+	{
 
 		cout << "itération : "<< count << ":: résidus : " << reste << ":: x :" << calcul_norme(vecteur_x, n) << ":: y :" << calcul_norme(vecteur_y, n) << endl;
 
+		//calcul X^k+1
 		matrice.produitVecteur(vecteur_x, vecteur_xkg);
 		produit_nabla_norme(vecteur_min_nabla, calcul_norme(vecteur_x, n),n, nablax);
 		somme_vect(vecteur_xkg, nablax, n);
 
+		//calcul Y^k+1
 		matrice.produitVecteur(vecteur_y, vecteur_ykg);
 		produit_nabla_norme(vecteur_min_nabla, calcul_norme(vecteur_y, n),n, nablay);
 		somme_vect(vecteur_ykg, nablay, matrice.size());
@@ -592,40 +415,6 @@ int main(int argc, char **argv)
 		count ++;
 	}
 	cout << "itération : "<< count << ":: résidus : " << reste << endl;
-/*
-	norme_x = calcul_norme(vecteur_x, line_number);
-	norme_y = calcul_norme(vecteur_y, line_number);
-	
-	cout << "norme x : " << norme_x << " norme_y : " << norme_y << endl;
-	cout << endl;
-	cout << "norme x : " << calcul_norme(matrice.getVecteurMax(), matrice.size()) << " norme_y : " << calcul_norme(matrice.getVecteurMin(), matrice.size()) << endl;
-
-	norme_x = 1-norme_x;
-	norme_y = 1-norme_y;
-	
-	cout << "norme x : " << norme_x << " norme_y : " << norme_y << endl;
-	
-	//tester pour s'assurer qu'il y a bien surcharge d'opérateur sur l'affectation, et pas que seuls les pointeurs sont copiés......
-	vecteur_x_tmp = produit_nabla_norme(vecteur_min_nabla, norme_x, line_number);
-	vecteur_y_tmp = produit_nabla_norme(vecteur_min_nabla, norme_y, line_number);
-	*/
-
-
-
-	//a faire
-	// XkG = produit(vecteur_x, G)
-	// XkG += somme_vect(XkG, produit_nabla_norme(vecteur_min_nabla, norme_x, line_number));
-	// YkG = produit(vecteur_y, G)
-	// YkG += somme_vect(YkG, produit_nabla_norme(vecteur_min_nabla, norme_y, line_number))
-	// boucle
-	//		|vecteur_x[i] = max(vecteur_x[i], XkG[i])
-	//		|vecteur_y[i] = min(vecteur_y[i], YkG[i])
-	// fin boucle
-	//
-	// 
-	// continuer tant que truc epsilon faux
-	
-
 	
 	return 0;
 }
